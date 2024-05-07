c500368f339521862c768771e9de7702c256db0f
nabilasaf <nabilasafitri861@gmail.com>
2024-05-07 14:51:58 +0700

    mempublikasikan tinggi,alas,dan luas segitiga

--- pertama.cpp
+++ pertama.cpp
@@ -14,4 +14,11 @@ class peersegiPanjang
 class segitiga 
 {
     public:
+       int tinggi,
+           alas;
+        int luasSegitiga(int a, int t)
+        {
+            return 0.5 * a * t;
+        }
+       
 }
