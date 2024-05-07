d91f0f28eeafd1997d1b768291711ec11d280b07
nabilasaf <nabilasafitri861@gmail.com>
2024-05-07 15:19:15 +0700

    isi tampilan pointer_1

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -11,5 +11,15 @@ class mahasiswa {
 };
 
 int main(){
-    
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Poineter reference refMhs
+    refMhs.nim = 2; //Member access operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //Pointer dereference pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    return 0;
 }
